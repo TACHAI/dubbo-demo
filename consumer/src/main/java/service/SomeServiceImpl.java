9644bc77bf5a4a9b4140425e41446191191d270c
tachai <1206966083@qq.com>
2020-10-08 20:46:26 +0800

    生产者，消费者

---
+++ consumer/src/main/java/service/SomeServiceImpl.java
@@ -0,0 +1,16 @@
+package service;
+
+/**
+ * Create by tachai on 2020-10-08 20:35
+ * gitHub https://github.com/TACHAI
+ * Email tc1206966083@gmail.com
+ */
+public class SomeServiceImpl  implements SomeService{
+    @Override
+    public String hello(String name) {
+
+        System.out.println(name+",我是消费者");
+
+        return "Hello Dubbo World! "+name;
+    }
+}
