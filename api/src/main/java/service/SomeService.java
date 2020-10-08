609d3aa06bb90c1e87484e80ca751df72deb10fb
tachai <1206966083@qq.com>
2020-10-08 20:24:08 +0800

    学习dubbo:框架搭建

---
+++ api/src/main/java/service/SomeService.java
@@ -0,0 +1,10 @@
+package service;
+
+/**
+ * Create by tachai on 2020-10-08 20:17
+ * gitHub https://github.com/TACHAI
+ * Email tc1206966083@gmail.com
+ */
+public interface SomeService {
+    String hello(String name);
+}
